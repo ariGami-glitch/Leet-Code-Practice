920ebef5497d3f35d5190ab3d708b9b8c9c35595
Ariana Tran <ariana192000@hotmail.com>
2021-08-12 10:51:13 -0400

    added to main and modify the method

--- DelDuplicate.java
+++ DelDuplicate.java
@@ -1,2 +1,14 @@
 public class DelDuplicate {
+    public DelDuplicate() {
+        System.out.println("This object aims to delete the duplicates in arrays.");
+    }
+    public int removeDuplicates(int[] nums) {
+        int length = 0;
+        int i = 0;
+        int j;
+
+
+        return length;
+    }
+
 }
--- LeetMain.java
+++ LeetMain.java
@@ -1,11 +1,7 @@
 public class LeetMain {
     public static void main(String[] args) {
-        int[] arr = new int[]{17, 18, 1};
-        GreatestReplace gr = new GreatestReplace();
-        arr = gr.efficient(arr);
-        for(int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + " ");
-        }
+        DelDuplicate dd = new DelDuplicate();
+        int[] arr = new int[]{1, 2, 2, 3};
 
     }
 }
