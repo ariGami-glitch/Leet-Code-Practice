5d5a33e5293e4697cee65792233f98a284c71cc5
Ariana Tran <atran2@mail.umw.edu>
2024-01-22 21:48:33 -0500

    finished implementing a graph data structure

---
+++ graphs/GraphTraverser.java
@@ -0,0 +1,11 @@
+package graphs;
+
+public class GraphTraverser {
+    // Traversing a graph has a runtime of O (Vertices + Edges)
+    // depth first search sorts the vertices
+    // 3 ways to sort a graph
+
+    public static void main(String[] args) {
+
+    }
+}
